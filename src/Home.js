7494c03a99d65466a4a801dc447fbb1416fa472a
uvin6667 <uvin6667@gmail.com>
2021-06-21 22:17:06 +0530

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,9 @@ const Home = () => {
         {title: 'My new website', body: 'lorem ipsum..', author: 'mario', id: 1},
         {title: 'Welcome party', body: 'lorem ipsum..', author: 'yoshi', id: 2},
         {title: 'Web dev top tips', body: 'lorem ipsum..', author: 'mario', id: 3},
-    ])
+    ]);
+
+    const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,12 +16,15 @@ const Home = () => {
     }
 
     useEffect(()=>{
-        console.log('use effect ran')
-    });
+        console.log('use effect ran');
+        console.log(name);
+    },[name]);
 
     return (  
         <div className="home">
            <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
+           <button onClick={()=> setName('luigi')}>Change name</button>
+           <p>{name}</p>
         </div>
     );
 }
