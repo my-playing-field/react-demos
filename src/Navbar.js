b049f822bcfddc5dfaa4278092dd5af23d3a860d
uvin6667 <uvin6667@gmail.com>
2021-06-23 23:51:24 +0530

    Exact match routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 const Navbar = () => {
-    return ( 
+    return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/course" >New Blog</a>
+                <a href="/create" >New Blog</a>
             </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
