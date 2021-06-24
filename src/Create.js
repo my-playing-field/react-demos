7c94f844123670fde9d3dd0b1bd79f7163df3f38
uvin6667 <uvin6667@gmail.com>
2021-06-24 14:01:13 +0530

    Deleting blogs

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending,setIsPending]=useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,8 +19,8 @@ const Create = () => {
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify(blog)
         }).then(()=>{
-            console.log('new blog added');
             setIsPending(false);
+            history.push('/');
         })
     }
 
